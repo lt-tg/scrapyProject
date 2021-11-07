fbd2401d8d0fbfec8dd7c293ac9e31408fe1c446
lt-tg <841241285@qq.com>
2021-11-07 21:36:22 +0800

    Extract the URL list of the news page and the URL of the next page.

--- blogsSpider/blogsSpider/spiders/cnblogs.py
+++ blogsSpider/blogsSpider/spiders/cnblogs.py
@@ -1,5 +1,7 @@
 import scrapy
 from undetected_chromedriver import v2 as uc
+from scrapy import Request
+from urllib import parse
 
 class CnblogsSpider(scrapy.Spider):
     name = 'cnblogs'
@@ -8,6 +10,7 @@ class CnblogsSpider(scrapy.Spider):
     custom_settings = {
         "COOKIES_ENABLED": True
     }
+
     def start_requests(self):
         #入口模拟登录，获得cookie
         driver = uc.Chrome()
@@ -21,10 +24,27 @@ class CnblogsSpider(scrapy.Spider):
             headers = {
                 'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
             }
-            yield scrapy.Request(url, cookies=cookie_dic, headers=headers, dont_filter=True)
+            yield Request(url, cookies=cookie_dic, headers=headers, dont_filter=True)
 
     def parse(self, response):
-        url_list = response.xpath("//div[@id='news_list']//h2[@class='news_entry']/a/@href").extract()
-        url_list = response.css('div#news_list h2 a::attr(href)').extract()
-        print(url_list)
+        """
+        1.提取本页面中的新闻url列表
+        2.提取下一个页面的url
+        3.提取新闻列表中带封面的新闻图片url
+        """
+        post_nodes = response.css("#news_list .news_block")
+        for node in post_nodes:
+            img_url = node.css(".entry_summary a img::attr(href)").extract_first("")
+            post_url = node.css("h2 a::attr(href)").extract_first("")
+            yield Request(url=parse.urljoin(response.url, post_url), meta={"front_image_url": img_url}, callback=self.parse_detail)
+
+        # next_url = response.css("div.pager a:last-child::attr(text)").extract_frist("")
+        # if next_url == "Next >":
+        #     next_url = response.css("div.pager a:last-child::attr(href)").extract_frist("")
+        #     yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
+        next_url = response.xpath("//a[contains(text(), 'Next >']/@href").extract_first("")
+        yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
+
+    def parse_detail(self, response):
         pass
+
